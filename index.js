9ceab5b491cef0068c9a3b7ddfb911b6e6fdd372
Darnell Harris <d.michael.harri92@gmail.com>
2019-07-22 21:30:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 
-seyHiToGrandma(string){
-  
+sayHiToGrandma(string){
+  if(string.toLowerCase() === string){
+    return "I can't hear you!";
+  }else if(string.toUpperCase() === string){
+    return "YES INDEED!";
+  }else if(string === "I love you, Grandma."){
+    return "I love you, too.";
+  }
 }
+
